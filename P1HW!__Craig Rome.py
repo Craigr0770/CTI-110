986b1367aba219cb1547622d8e9a4fc696dbfcd0
Craigr0770 <146122268+Craigr0770@users.noreply.github.com>
2023-09-26 14:21:47 -0400

    Add files via upload

---
+++ P1HW!__Craig Rome.py
@@ -0,0 +1,31 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: C:/Users/craigr0770/Downloads/P1HW1_CraigRome.py ==========
+Enter a number 4
+you entereted  4
+4 squared is  16
+And 4 cubed is 64
+>>> 
+>>> 
+>>> 
+=========== RESTART: C:/Users/craigr0770/Downloads/P1HW1_CraigRome.py ==========
+Enter a number 10000000000
+you entereted  10000000000
+10000000000 squared is  100000000000000000000
+And 10000000000 cubed is 1000000000000000000000000000000
+>>> 
+=========== RESTART: C:/Users/craigr0770/Downloads/P1HW1_CraigRome.py ==========
+Enter a number 0
+you entereted  0
+0 squared is  0
+And 0 cubed is 0
+>>> 
+=========== RESTART: C:/Users/craigr0770/Downloads/P1HW1_CraigRome.py ==========
+Enter a number: 8
+you entereted  8
+8 squared is  64
+And 8 cubed is 512
+Enter another number: 93
+8 + 93 = 101
+8 * 93 = 744
