1839094fe3191d194ae5ba7b2943791a8ab6dfaa
Craigr0770 <146122268+Craigr0770@users.noreply.github.com>
2023-09-26 14:37:27 -0400

    Add files via upload

---
+++ P1HW2_CraigRome.py
@@ -0,0 +1,28 @@
+# Rome Craig
+# 9/26/2023
+# Calculate travel epenses
+
+budget = int(input("Enter Budget: "))
+
+dest = input("Where are you traveling? ")
+
+gas = int(input("Enter gas Budget: "))
+
+food = int(input("Enter gas Budget: "))
+
+hotel = int(input("Enter gas Budget: "))
+
+expenses = gas+food+hotel
+
+
+print("-------Travel Expenses------")
+print("Location: ", dest)
+print("Inital Budget: ", budget)
+print()
+print("Gas Cost: ", gas)
+print("Gas Food: ", food)
+print("Gas hotel: ", hotel)
+print()
+print("Remaining Balance: ", buget-expense)
+
+      
